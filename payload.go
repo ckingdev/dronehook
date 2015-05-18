9a1ad6d51505ae7950575cdccb21437331d05271
Cameron Palone <mcameronpalone@gmail.com>
2015-05-18 13:58:45 -0600

    Initial commit.

---
+++ payload.go
@@ -0,0 +1,65 @@
+package dronehook
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type RepositoryType struct {
+	Remote       string `json:"remote"`
+	Host         string `json:"host"`
+	Owner        string `json:"owner"`
+	Name         string `json:"name"`
+	URL          string `json:"url"`
+	CloneURL     string `json:"clone_url"`
+	GitURL       string `json:"git_url"`
+	SSHURL       string `json:"ssh_url"`
+	Active       bool   `json:"active"`
+	Private      bool   `json:"private"`
+	Privileged   bool   `json:"privileged"`
+	PostCommits  bool   `json:"post_commits"`
+	PullRequests bool   `json:"pull_requests"`
+	Timeout      int    `json:"timeout"`
+	CreatedAt    int    `json:"created_at"`
+	UpdatedAt    int    `json:"updated_at"`
+}
+
+type CommitType struct {
+	ID          int    `json:"id"`
+	Status      string `json:"status"`
+	StartedAt   int    `json:"started_at"`
+	FinishedAt  int    `json:"finished_at"`
+	Duration    int    `json:"duration"`
+	SHA         string `json:"sha"`
+	Branch      string `json:"branch"`
+	PullRequest string `json:"pull_request"`
+	Author      string `json:"author"`
+	Gravatar    string `json:"gravatar"`
+	Timestamp   string `json:"timestamp"`
+	Message     string `json:"message"`
+	CreatedAt   int    `json:"created_at"`
+	UpdatedAt   int    `json:"updated_at"`
+}
+
+type Payload struct {
+	FromURL    string         `json:"from_url"`
+	Commit     CommitType     `json:"commit"`
+	Repository RepositoryType `json:"repository"`
+}
+
+func makePayload(raw []byte) (*Payload, error) {
+	var p Payload
+	if err := json.Unmarshal(raw, &p); err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func (p *Payload) String() string {
+	return fmt.Sprintf("[ drone.io | %s | %s ] %s | %s",
+		p.Repository.Name,
+		p.Commit.Branch,
+		p.Commit.Message,
+		p.Commit.Status,
+	)
+}
---
+++ server.go
@@ -0,0 +1,64 @@
+package dronehook
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	Port int
+	Path string
+	Out  chan Payload
+}
+
+func NewServer(port int, path string) *Server {
+	return &Server{
+		Port: port,
+		Path: path,
+		Out:  make(chan Payload),
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+
+	if req.Method != "POST" {
+		http.Error(w, "405 Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if req.URL.Path != s.Path {
+		http.Error(w, "404 Not found- expected "+s.Path, http.StatusNotFound)
+		return
+	}
+
+	respBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "500 Internal Error - Could not read from request: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	go s.processPayload(respBody)
+}
+
+func (s *Server) processPayload(raw []byte) {
+	p, err := makePayload(raw)
+	if err != nil {
+		log.Printf("Error making payload: %s", err)
+		return
+	}
+	s.Out <- *p
+}
+
+func (s *Server) ListenAndServe() error {
+	return http.ListenAndServe(fmt.Sprintf(":%v", s.Port), s)
+}
+
+func (s *Server) GoListenAndServe() {
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	}()
+}
